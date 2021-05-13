2a282704b069f5f0105b1f3268a3432e133a5606
Vishal mangade <vishalvirupanmangade@gmail.com>
2021-05-14 01:18:15 +0530

    starting project

---
+++ jee-main/full-syllabus-test.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Mangade: IIT-JEE/NEET/CET</title>
+</head>
+
+<body>
+    <?php include "php/navbar.php"; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus-test/test-1.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 1</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus-test/test-2.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 2</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus-test/test-3.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 3</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus-test/test-4.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 4</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus-test/test-5.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 5</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 6</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 7</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 8</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 9</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 10</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+            <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 11</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+            <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Full Syllabus Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">Test 12</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ jee-main/previous-year-test.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Mangade: IIT-JEE/NEET/CET</title>
+</head>
+
+<body>
+    <?php include "php/navbar.php"; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: March Test 1 </h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: March Test 2</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: March Test 3</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: March Test 4</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: March Test 5</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: March Test 6</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: February Test 1</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: February Test 2</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: February Test 3</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: February Test 4</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: February Test 5</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card text-white bg-primary m-3">
+                    <div class="card-header">JEE Main: Previous Year Test </div>
+                    <div class="card-body">
+                        <a href="full-syllabus.php" style="text-decoration: none;color:white;">
+                            <h5 class="card-title">2021: February Test 6</h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
